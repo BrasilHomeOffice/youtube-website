a649f4d22f63e83ec9b33ee633d18434558f43f0
Giovanne Afonso <giovanneafonso@gmail.com>
2021-04-04 10:50:15 -0300

    Home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,64 +1,47 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Brasil Home Office - Youtube</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Brasil Home Office
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Lista de vídeos
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <ul className={styles.listOfVideos}>
+          <li>
+            <a href="#">Apresentação - O que é Brasil Home Office</a>
+          </li>
+          <li>
+            <a href="#">Que profissões Home Office existem?!</a>
+          </li>
+          <li>
+            <a href="#">Profissões Home Office sem computador</a>
+          </li>
+          <li>
+            <a href="#">Como criar uma empresa home office</a>
+          </li>
+          <li>
+            <a href="#">Programador - Profissão Home Office</a>
+          </li>
+          <li>
+            <a href="#">Editor de Vĩdeo - Profissão Home Office</a>
+          </li>
+        </ul>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        Site para o canal do youtube <a href="#">Brasil Home Office</a>
       </footer>
     </div>
   )
